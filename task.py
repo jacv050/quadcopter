1ed833ac818ee58e685a34aa487379d64d86c53f
jacv050 <jacv050@gmail.com>
2018-04-11 15:30:11 +0200

    Quadcopter solution

--- task.py
+++ task.py
@@ -47,3 +47,121 @@ class Task():
         self.sim.reset()
         state = np.concatenate([self.sim.pose] * self.action_repeat) 
         return state
+
+
+class Takeoff():
+    """Task (environment) that defines the goal and provides feedback to the agent."""
+    def __init__(self, init_pose=None, init_velocities=None, 
+        init_angle_velocities=None, runtime=5., height=None):
+        """Initialize a Task object.
+        Params
+        ======
+            init_pose: initial position of the quadcopter in (x,y,z) dimensions and the Euler angles
+            init_velocities: initial velocity of the quadcopter in (x,y,z) dimensions
+            init_angle_velocities: initial radians/second for each of the three Euler angles
+            runtime: time limit for each episode
+            target_pos: target/goal (x,y,z) position for the agent
+        """
+        # Simulation
+        self.sim = PhysicsSim(init_pose, init_velocities, init_angle_velocities, runtime) 
+        self.action_repeat = 30
+
+        self.state_size = self.action_repeat * 6
+        self.action_low = 0
+        self.action_high = 900
+        self.action_size = 4
+
+        self.max_reward = -np.inf
+        # Goal
+        self.target_pos = [0., 0., height] if height is not None else np.array([0., 0., 10.]) 
+
+    def get_reward(self):
+        """Uses current pose of sim to return reward."""
+        #height_reward = np.exp(-abs(self.sim.pose[2] - self.target_pos[2]))
+        
+        #error = 1 * np.exp(-abs(self.sim.pose[2] - self.target_pos[2]))
+        #reward = np.tanh(1 - np.exp(-1/(abs(self.sim.pose[2] - self.target_pos[2])/self.target_pos[2])))
+        #reward = np.exp(-np.sqrt((abs(self.sim.pose[2] - self.target_pos[2]))))
+        reward = (1-(abs(self.sim.pose[2] - self.target_pos[2]))/self.target_pos[2])
+        #if reward < 0.1:
+        #    reward += 20
+        #self.max_reward = max(self.max_reward, reward)
+        #phi_error = 0.1*(1 - np.exp(-1/self.sim.pose[3]))
+        #theta_error = 0.1*(1 - np.exp(-1/self.sim.pose[4]))
+        
+        #phi_error = 0.000004 * (1 - abs(np.sin(self.sim.pose[3])))
+        #theta_error = 0.000004 * (1 - abs(np.sin(self.sim.pose[4])))
+        
+        #penalty_phi = np.exp(-abs(self.sim.pose[3] - 0)*0.01)+0.5
+        #penalty_theta = np.exp(-abs(self.sim.pose[4] - 0)*0.01)+0.5
+        
+        #reward = np.exp(-abs(self.sim.pose[3] - 0))/2 #phi
+        #reward += np.exp(-abs(self.sim.pose[4] - 0))/2 #theta
+        #reward = np.tanh(reward/3*2-1)
+        
+        return reward #+ phi_error + theta_error
+
+    def step(self, rotor_speeds):
+        """Uses action to obtain next state, reward, done."""
+        reward = 0
+        pose_all = []
+        for _ in range(self.action_repeat):
+            done = self.sim.next_timestep(rotor_speeds) # update the sim pose and velocities
+            reward += self.get_reward() 
+            pose_all.append(self.sim.pose)
+        next_state = np.concatenate(pose_all)
+        return next_state, reward, done
+
+    def reset(self):
+        """Reset the sim to start a new episode."""
+        self.sim.reset()
+        state = np.concatenate([self.sim.pose] * self.action_repeat) 
+        return state
+
+    
+class Hover():
+    """Task (environment) that defines the goal and provides feedback to the agent."""
+    def __init__(self, init_pose=None, init_velocities=None, 
+        init_angle_velocities=None, runtime=5.):
+        """Initialize a Task object.
+        Params
+        ======
+            init_pose: initial position of the quadcopter in (x,y,z) dimensions and the Euler angles
+            init_velocities: initial velocity of the quadcopter in (x,y,z) dimensions
+            init_angle_velocities: initial radians/second for each of the three Euler angles
+            runtime: time limit for each episode
+            target_pos: target/goal (x,y,z) position for the agent
+        """
+        # Simulation
+        self.sim = PhysicsSim(init_pose, init_velocities, init_angle_velocities, runtime) 
+        self.action_repeat = 3
+
+        self.state_size = self.action_repeat * 6
+        self.action_low = 0
+        self.action_high = 900
+        self.action_size = 4
+
+        # Goal - Hover in initial pose
+        self.target_pose = init_pose
+
+    def get_reward(self):
+        """Uses current pose of sim to return reward."""
+        reward = 1.-.3*(abs(self.sim.pose[:3] - self.target_pos)).sum()
+        return reward
+
+    def step(self, rotor_speeds):
+        """Uses action to obtain next state, reward, done."""
+        reward = 0
+        pose_all = []
+        for _ in range(self.action_repeat):
+            done = self.sim.next_timestep(rotor_speeds) # update the sim pose and velocities
+            reward += self.get_reward() 
+            pose_all.append(self.sim.pose)
+        next_state = np.concatenate(pose_all)
+        return next_state, reward, done
+
+    def reset(self):
+        """Reset the sim to start a new episode."""
+        self.sim.reset()
+        state = np.concatenate([self.sim.pose] * self.action_repeat) 
+        return state
